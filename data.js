33003efa0429ed783cfeda929d0c8825ccae8bdc
SkorobogatovIegor <skorobogatov.iegor@yandex.ru>
2026-04-19 21:47:37 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,10 @@
+const org = [
+    {id: 1, title: "ржд",     value: 1000,    status: "trusted",          createdAt: "2026-03-22"},
+    {id: 2, title: "авиасейлс",       value: 1200,     status: "under_review",     createdAt: "2026-03-23"},
+    {id: 3, title: "яхты на прокат",       value: 2000,    status: "new",              createdAt: "2026-03-24"},
+    {id: 4, title: "ремонт электронники",       value: 1800,     status: "trusted",          createdAt: "2026-03-25"},
+    {id: 5, title: "риэлторное агенство",     value: 400,    status: "trusted",          createdAt: "2026-03-26"},
+    {id: 6, title: "кондитерская",       value: 6000,     status: "untrusted",        createdAt: "2026-03-27"},
+    {id: 7, title: "настольные игры",    value: 300,     status: "new",              createdAt: "2026-03-29"},
+    {id: 8, title: "клининг",          value: 700,     status: "new",              createdAt: "2026-04-05"},
+];
